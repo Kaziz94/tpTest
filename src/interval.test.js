c25776f3ca0242060e5b48041cb015a183761ede
Kaziz <david_gola@outlook.fr>
2019-09-26 17:02:09 +0200

    includes

--- src/interval.test.js
+++ src/interval.test.js
@@ -12,4 +12,17 @@ describe('pairs', function () {
             expect(interval.overlaps(n)).toBe(expected);
         }
     );
+
+    test.each([
+        [new Interval(5,6), true],
+        [new Interval(3,12), false],
+        [new Interval(4,7), true],
+        [new Interval(8,10), false],
+        
+    ])(
+        'includes %s %p',
+        (n, expected) => {
+            expect(interval.includes(n)).toBe(expected);
+        }
+    );
 });
